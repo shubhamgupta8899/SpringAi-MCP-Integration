cc41ecb5bc5938ef10738620afe8430d1842db99
shubhamgupta8899 <shubhamkrgupta2026@gmail.com>
2026-06-08 02:13:09 +0530

    Implement FileSystem MCP that can manage the file...

---
+++ src/main/java/com/shubham/SpringAi_MCP/controller/MCPController.java
@@ -0,0 +1,36 @@
+package com.shubham.SpringAi_MCP.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MCPController {
+
+    private final ChatClient chatClient;
+
+    private final ToolCallbackProvider mcpTool;
+
+    public MCPController(ChatClient.Builder chatClientBuilder,  ToolCallbackProvider mcpTool){
+        this.mcpTool = mcpTool;
+        this.chatClient = chatClientBuilder
+                .defaultToolCallbacks(mcpTool)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String question){
+
+        return chatClient.prompt()
+                .user(question)
+                .call()
+                .content();
+
+    }
+}
---
+++ src/test/java/com/shubham/SpringAi_MCP/SpringAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.SpringAi_MCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
